4a58a4e3d2487afa1bfa4659932ec3e69d2a5b1b
LarsZauberer <wasser.ian@gmail.com>
2022-12-05 19:44:47 +0100

    All simple views added

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('app', app, name="app"),
+    path('menu', menu, name="menu"),
+    path('allMenu', allMenu, name="allMenu"),
     path('', index, name="index")
 ]
