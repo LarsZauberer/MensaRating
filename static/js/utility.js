589fa9e4677f918895ead39fb79cf7d7d7082a04
RobRob1101 <robin.steiner00@gmail.com>
2023-02-06 22:35:09 +0100

    give stars implemented

--- static/js/utility.js
+++ static/js/utility.js
@@ -8,3 +8,20 @@ function starRate(index, rating){
 
     star.style.width = rating
 }
+
+function starHover(stars, i){
+
+    for(let j = 0; j <= i; j++){
+        stars[j].style.backgroundImage = "url('../../static/images/yellowstar.png')"
+
+
+    }
+
+
+}
+
+function stopStarHover(){
+    for(let j = 0; j < 5; j++){
+        stars[j].style.backgroundImage = "url('../../static/images/emptystar.png')"
+    }
+}
