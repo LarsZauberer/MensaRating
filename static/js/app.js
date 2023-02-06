e9c51475f3de38615dd8ac7c05a0948fc4be2a6b
LarsZauberer <wasser.ian@gmail.com>
2023-02-06 12:10:28 +0100

    liking function in the frontend

--- static/js/app.js
+++ static/js/app.js
@@ -10,4 +10,50 @@ document.addEventListener("DOMContentLoaded", function (e) {
             return;
         }
     }
+
+    update_all_btn();
 })
+
+function update_all_btn() {
+    likeButtons = document.getElementsByClassName("like-btn")
+    for (let index = 0; index < likeButtons.length; index++) {
+        const element = likeButtons[index];
+        id = element.id.replace("-btn", "")
+        post = localStorage.getItem(id);
+        if (post) {
+            element.innerHTML = "Dislike"
+            if (!element.className.includes("disliked")) {
+                element.className = element.className.replace(" liked", "")
+                element.className += " disliked"
+            }
+        } else {
+            if (!element.className.includes("liked")) {
+                element.innerHTML = "Like"
+                element.className = element.className.replace(" disliked", "")
+                element.className += " liked"
+            }
+        }
+    }
+}
+
+function like(cat, pk) {
+    const post = localStorage.getItem(cat + "-" + pk);
+
+    change_like = function() {
+        console.log(this.responseText);
+        if (this.status == 200) {
+            document.getElementById(cat + "-" + pk).innerHTML = this.responseText
+        }
+    }
+
+    if (post) {
+        // Dislike
+        api.get("like/" + cat + "/" + pk, "?dislike=YES", change_like);
+        localStorage.removeItem(cat + "-" + pk);
+        update_all_btn();
+    } else {
+        api.get("like/" + cat + "/" + pk, "", change_like);
+        localStorage.setItem(cat + "-" + pk, "YES");
+        update_all_btn();
+    }
+}
