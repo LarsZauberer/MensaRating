cee73a657e45e93fc25c8978fc51e409573795b5
RobRob1101 <robin.steiner00@gmail.com>
2023-02-12 19:31:07 +0100

    order-buttons working

--- static/js/statistics.js
+++ static/js/statistics.js
@@ -1,14 +1,41 @@
-function orderMenutypes(menuTypes){
-    menuTypes.sort(function(a, b){
-        let x = a.occurrences;
-        let y = b.occurrences;
+function orderMenutypes(menuTypes, orderType){
+    if(orderType == "occ"){
+        menuTypes.sort(function(a, b){
+            let x = a.occurrences;
+            let y = b.occurrences;
 
-        if(x < y) return 1;
-        else if(x > y) return -1;
-        return 0;
+            if(x < y) return 1;
+            else if(x > y) return -1;
+            return 0;
+        })
+    }
+    else if(orderType == "rating"){
+        menuTypes.sort(function(a, b){
+            let x = a.rating;
+            let y = b.rating;
+
+            if(x < y) return 1;
+            else if(x > y) return -1;
+            return 0;
+        })
+    }
+    else if(orderType == "numrates"){
+        menuTypes.sort(function(a, b){
+            let x = parseInt(a.numrates);
+            let y = parseInt(b.numrates);
+
+            if(x < y) return 1;
+            else if(x > y) return -1;
+            return 0;
+        })
+    }
+
+    
+    
 
 
-    })
+    console.log(menuTypes)
+
 
     for(let i = 0; i < menuTypes.length; i++){
         let menuTypeObject = document.getElementById("menutype-" + i);
@@ -22,14 +49,13 @@ function orderMenutypes(menuTypes){
         menuTypeObject[0].innerHTML = menuType.name
 
         let vegivegan = menuTypeObject[1].children;
+
+        vegivegan[0].style.width = vegivegan[0].style.height = "0";
+        vegivegan[1].style.width =  vegivegan[1].style.height = "0";
         if(menuType.vegetarian == "True"){
-            vegivegan[0].style.width = "100%";
-            vegivegan[0].style.height = "100%";
-            vegivegan[0].style.opacity = "1";
+            vegivegan[0].style.width = vegivegan[0].style.height = "100%";
         }else if(menuType.vegan == "True"){
-            vegivegan[1].style.width = "100%";
-            vegivegan[1].style.height = "100%";
-            vegivegan[1].style.opacity = "1";
+            vegivegan[1].style.width =  vegivegan[1].style.height = "100%";
         }
 
         menuTypeObject[2].innerHTML = "Vorkommen: " + menuType.occurrences
@@ -41,24 +67,19 @@ function orderMenutypes(menuTypes){
         starRate(i, menuType.rating)
         rating[2].innerHTML = "(" + menuType.numrates + ")"
     }
+
+    toggleOrderOptions()
 }
 
 function toggleOrderOptions(){
-    
-
     let orderoptions = document.getElementById("sort-criteria")
     let orderButtons = orderoptions.children
-
-
-    if(orderoptions.style.height == "" ||orderoptions.style.height == "0em"){
+    if(orderoptions.style.height == "" || orderoptions.style.height == "0em"){
         orderoptions.style.height = "1em"
         for(let i = 0; i < orderButtons.length; i++){
-            console.log(orderButtons[i].style.height)
             orderButtons[i].style.height = "2em"
             orderButtons[i].style.opacity = 1;
         }
-
-
     }
     else{
         orderoptions.style.height = "0em"
