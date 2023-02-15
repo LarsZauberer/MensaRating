9bb5e42a9e2abf4d0a82b71cde3ffb61c27860fa
LarsZauberer <wasser.ian@gmail.com>
2023-02-15 08:02:56 +0100

    allowed hosts update

--- Web/settings.py
+++ Web/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-56a%ng1+!7w&jg#cl^ybg(ss%-=pmm(wv+oo_xb7c)+@p7*y*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', "*"]
 
 
 # Application definition
