98303b35a89ffcf1e6d656719126c4f6fd960f23
LarsZauberer <wasser.ian@gmail.com>
2023-02-15 09:14:16 +0100

    typo fix

--- core/urls.py
+++ core/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
 
 if settings.DEBUG:  # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_roott=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     
