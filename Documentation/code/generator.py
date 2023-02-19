3843b301a3fd0e7059f49e8bbaa5b1d4ee6d848d
LarsZauberer <wasser.ian@gmail.com>
2023-02-19 18:29:50 +0100

    improved code generator

--- Documentation/code/generator.py
+++ Documentation/code/generator.py
@@ -5,19 +5,30 @@ LIST_OF_FILES = [
     Path("core/webscraper.py"),
     Path("core/views.py"),
     Path("core/urls.py"),
-    # Path("users/views.py"),
+    Path("users/views.py"),
 ]
 
 
 for i in LIST_OF_FILES:
     with open(i, 'r') as f:
         content = f.read()
-        with open("Documentation/code/" + i.stem + ".tex", "w") as c:
+        name = str(i).replace("/", ".")
+        
+        # Extra characters removal
+        content = content.replace("ä", "ae")
+        content = content.replace("ö", "oe")
+        content = content.replace("ü", "ue")
+        content = content.replace("Ä", "Ae")
+        content = content.replace("Ö", "Oe")
+        content = content.replace("Ü", "Ue")
+        
+        with open("Documentation/code/" + name + ".tex", "w") as c:
             c.write("\\begin{lstlisting}[language=Python]\n")
             c.write(content)
             c.write("\n\\end{lstlisting}")
 
 with open("Documentation/code/main_code.tex", "w") as f:
     for i in LIST_OF_FILES:
-        f.write("\\section{" + i.stem + ".py}\\label{code:" + i.stem + ".py}\n")
-        f.write("\\input{code/" + i.stem + "}\n")
+        name = str(i).replace("/", ".")
+        f.write("\\section{" + name + "}\\label{code:" + name + "}\n")
+        f.write("\\input{code/" + name + "}\n")
