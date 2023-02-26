944df9da599b27f61d492e755de92963ee7a79b0
LarsZauberer <wasser.ian@gmail.com>
2023-02-26 17:15:40 +0100

    User Django App kommentieren Fixes #12

--- users/forms.py
+++ users/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
+# The user creation form
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    # promoCode = forms.CharField(max_length=10, required=False)
+    # promoCode = forms.CharField(max_length=10, required=False) ! Unused code
     
     class Meta:
         model = User
