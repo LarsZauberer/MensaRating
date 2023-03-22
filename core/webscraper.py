c9d613edba3e26c9c05569ec95bc4fe063c14942
ValentinGisi <valentin.gisi@gmx.ch>
2023-03-22 11:10:49 +0100

    fixed "&amp;" bug in webscraper

--- core/webscraper.py
+++ core/webscraper.py
@@ -49,7 +49,7 @@ def get_menu_list(days):
 
 def clean(str):
     # uses regex to substitute tags and double spaces in messy html string with empty strings or single spaces
-    string = re.sub('<p class="menu-description">|</p>|<h2 class="menu-title">|</h2>|\\xad\s*|\\n', '', str)
+    string = re.sub('<p class="menu-description">|</p>|<h2 class="menu-title">|</h2>|\\xad\s*|\\n|amp;', '', str)
     string = string.replace("<br/>", " ")
     return string.strip()
 
