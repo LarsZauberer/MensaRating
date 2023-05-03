0fe8c216aadcf3b6566e426e5491988aab682dd5
LarsZauberer <wasser.ian@gmail.com>
2023-05-03 10:52:46 +0200

    Try to fix the database overwriting bug with forcing order

--- core/webscraper.py
+++ core/webscraper.py
@@ -1,38 +1,43 @@
 # Maintained: Valentin (Scraping), Ian (Database Synchronization)
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
-from bs4 import BeautifulSoup  # To parse the html and analyze the data
-import requests  # To make a HTTP Request to the mensa page
-import re  # To use regex to scan the data
-from .models import Menu, MenuType  # To have access to the database
-import logging  # To gain logging information
 import datetime as dt  # To save information about the date
+import logging  # To gain logging information
+import re  # To use regex to scan the data
+
+import requests  # To make a HTTP Request to the mensa page
+from bs4 import BeautifulSoup  # To parse the html and analyze the data
+
+from .models import Menu, MenuType  # To have access to the database
 
 log = logging.getLogger("Webscraper")
 
-BLACKLIST = [
-    "Ferien",
-    "Kollegiumstag"
-]
+BLACKLIST = ["Ferien", "Kollegiumstag"]
+
 
 def get_menu_list(days):
     menuListDict = {}
 
     for menuIndex, day in enumerate(days):
         dishesOfDayDict = {}
-        dishes = day.find_all(attrs={"class":"menu-item"})
-        
+        dishes = day.find_all(attrs={"class": "menu-item"})
+
         for dishIndex, dish in enumerate(dishes):
             dishDict = {}
 
             # get title and description of dish with html tags as strings and using the "clean()" function to remove tags and double spaces
             title = clean(str(dish.find("h2")))
-            description = clean(str(dish.find(attrs={"class":"menu-description"})))
-            
+            description = clean(str(dish.find(attrs={"class": "menu-description"})))
+
             # find out if dish is served with meat (0), vegetarian (1) or vegan (2)
-            if (dish.find(attrs={"class":"label label-vegetarian has-infobox"}) is not None):
+            if (
+                dish.find(attrs={"class": "label label-vegetarian has-infobox"})
+                is not None
+            ):
                 label = 1
-            elif (dish.find(attrs={"class":"label label-vegan has-infobox"}) is not None):
+            elif (
+                dish.find(attrs={"class": "label label-vegan has-infobox"}) is not None
+            ):
                 label = 2
             else:
                 label = 0
@@ -41,34 +46,40 @@ def get_menu_list(days):
             dishDict["title"] = title
             dishDict["description"] = description
             dishDict["label"] = label
-            
+
             # add complete menu to the list
             dishesOfDayDict[f"dish{dishIndex}"] = dishDict
 
-        
         # add all menus of the day to the list
         menuListDict[f"menu{menuIndex}"] = dishesOfDayDict
-    
+
     # return list of every menu of every day
     return menuListDict
 
+
 def clean(str):
     # uses regex to substitute tags and double spaces in messy html string with empty strings or single spaces
-    string = re.sub('<p class="menu-description">|</p>|<h2 class="menu-title">|</h2>|\\xad\s*|\\n|amp;', '', str)
+    string = re.sub(
+        '<p class="menu-description">|</p>|<h2 class="menu-title">|</h2>|\\xad\s*|\\n|amp;',
+        "",
+        str,
+    )
     string = string.replace("<br/>", " ")
     return string.strip()
+
 
 def get_day_data():
     page = requests.get("https://neuekanti.sv-restaurant.ch/de/menuplan/")
     soup = BeautifulSoup(page.content, "html.parser")
-    return soup.find_all(attrs={"class":"menu-plan-grid"})
+    return soup.find_all(attrs={"class": "menu-plan-grid"})
+
 
 def get_dates():
     page = requests.get("https://neuekanti.sv-restaurant.ch/de/menuplan/")
     soup = BeautifulSoup(page.content, "html.parser")
     day_nav = soup.find_all(attrs={"class": "day-nav"})[0]
     dates = day_nav.find_all(attrs={"class": "date"})
-    
+
     for i, el in enumerate(dates):
         date = dt.datetime.strptime(el.contents[0], "%d.%m.").date()
         date_td = dt.date.today()
@@ -76,28 +87,31 @@ def get_dates():
         # check if this date is already happend -> Can happen at the end of the year
         # check if the date is more than 7 days in the past, so that this won't be triggered when the mensa page is not updated correctly.
         if date < date_td and date_td - date > dt.timedelta(days=7):
-            date = date.replace(year=date_td.year + 1) # set it to next year
+            date = date.replace(year=date_td.year + 1)  # set it to next year
         dates[i] = date
-    
+
     return dates
+
 
 def main():
     days = get_day_data()
     get_dates()
-    
+
     # Print
     for dishesOfDay in get_menu_list(days).values():
         for dish in dishesOfDay.values():
             for element in dish.values():
                 print(element)
             print("")
-    
+
     print(get_menu_list(days))
+
 
 def webscrape():
     days = get_day_data()  # Get all the days
     dates = get_dates()  # Get the dates of the days
     return get_menu_list(days), dates  # Retrive all the information
+
 
 def get_menu_type(title: str) -> MenuType:
     # Check if the menuType already exists
@@ -109,14 +123,17 @@ def get_menu_type(title: str) -> MenuType:
             menuType = [MenuType.objects.create(name=title)]
         except Exception as e:
             log.critical(f"Duplicate menuType")
-            log.critical(f"Debug information: title: {title}, description: {description}, label: {label}, date: {date}, menuType: {menuType}")
+            log.critical(
+                f"Debug information: title: {title}, description: {description}, label: {label}, date: {date}, menuType: {menuType}"
+            )
             return
         log.info(f"Created menuType: {menuType[0]}")
     return menuType[0]
 
+
 def create_menu_in_database(title, description, label, date):
     menuType = get_menu_type(title)
-    
+
     # Create the menu
     # Create the labels
     labels = [
@@ -124,12 +141,19 @@ def create_menu_in_database(title, description, label, date):
         {"vegetarian": True, "vegan": False},
         {"vegetarian": False, "vegan": True},
     ]
-    
-    Menu.objects.create(name=title, description=description, menuType=menuType, date=date, **labels[label])
+
+    Menu.objects.create(
+        name=title,
+        description=description,
+        menuType=menuType,
+        date=date,
+        **labels[label],
+    )
+
 
 def update_menu_in_database(title, description, label, date, menu):
     menuType = get_menu_type(title)
-    
+
     # Update the menu
     # Create the labels
     labels = [
@@ -137,7 +161,7 @@ def update_menu_in_database(title, description, label, date, menu):
         {"vegetarian": True, "vegan": False},
         {"vegetarian": False, "vegan": True},
     ]
-    
+
     menu.name = title
     menu.description = description
     menu.menuType = menuType
@@ -145,7 +169,7 @@ def update_menu_in_database(title, description, label, date, menu):
     menu.vegetarian = labels[label]["vegetarian"]
     menu.vegan = labels[label]["vegan"]
     menu.save()
-    
+
 
 def sync_today_menu():
     log.debug(f"Retrieving mensa data")
@@ -154,37 +178,50 @@ def sync_today_menu():
 
     for key, date in zip(data.keys(), dates):
         menus = Menu.objects.filter(date=date)
-        
+
         # Update the menus with the new data
         # If the menu is not in the database, create it
         # If the menu is in the database, update it
         # If the menu is not in the new data, delete it
-        
+
         while len(menus) > len(data[key].keys()):
             # There are more menus in the database than in the new data
             # -> Delete the menus that are not in the new data
             log.error(f"There are more menus in the database than in the new data")
-            menus[len(menus)-1].delete()
-            menus = Menu.objects.filter(date=date)
-            
-        
+            menus[len(menus) - 1].delete()
+            # menus = Menu.objects.filter(date=date)
+            menus = Menu.objects.filter(date=date).order_by("id")
+
         for index, item in enumerate(data[key].keys()):
             # Blacklist menus
-            if data[key][item]['title'] in BLACKLIST:
+            if data[key][item]["title"] in BLACKLIST:
                 continue
-            
+
             # Check if the menu is in the database
             try:
                 menu = menus[index]
-                log.debug(f"Menu \"{data[key][item]['title']}\" already in database (DB: {menu})")
-                update_menu_in_database(data[key][item]["title"], data[key][item]["description"], data[key][item]["label"], date, menu)  # Update the menu
+                log.debug(
+                    f"Menu \"{data[key][item]['title']}\" already in database (DB: {menu})"
+                )
+                update_menu_in_database(
+                    data[key][item]["title"],
+                    data[key][item]["description"],
+                    data[key][item]["label"],
+                    date,
+                    menu,
+                )  # Update the menu
                 log.debug(f"Updated menu: {menu}")
             except IndexError:
                 # Menu is not in the database -> create one
                 log.info(f"Menu \"{data[key][item]['title']}\" is not in the database")
-                create_menu_in_database(data[key][item]["title"], data[key][item]["description"], data[key][item]["label"], date)  # Create the menu
+                create_menu_in_database(
+                    data[key][item]["title"],
+                    data[key][item]["description"],
+                    data[key][item]["label"],
+                    date,
+                )  # Create the menu
                 log.info(f"Created menu: {data[key][item]['title']}")
-        
+
         # Cleanup
         # Delete menuTypes that have 0 occurences
         allMenuTypes = MenuType.objects.all()
