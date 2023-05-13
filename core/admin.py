5578475a550d6e4a501ea8ab99bc22acc7763769
LarsZauberer <wasser.ian@gmail.com>
2023-05-13 19:05:50 +0200

    CSS fixes and added to admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profil, MenuType, Menu, Review, Image, Rating, Badge
+from .models import Profil, MenuType, Menu, Review, Image, Rating, Badge, Memes
 
 admin.site.register(Profil)
 admin.site.register(Menu)
@@ -8,3 +8,4 @@ admin.site.register(Image)
 admin.site.register(Rating)
 admin.site.register(MenuType)
 admin.site.register(Badge)
+admin.site.register(Memes)
